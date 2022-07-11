068a202bccb0339e643e37c1fd3d97199e789466
name <meganjen12345@gmail.com>
2022-07-11 00:53:53 -0700

    ur short

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,12 +3,11 @@ package main
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/meganjen8888/acupuncture/pkg/config"
 	"github.com/meganjen8888/acupuncture/pkg/handlers"
 	"net/http"
 )
 
-func routes(app *config.AppConfig) http.Handler {
+func routes() http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
@@ -17,6 +16,9 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
